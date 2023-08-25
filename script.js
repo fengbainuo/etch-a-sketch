b4770d40fc4ae4f48a2c85e5058d412060ac9b87
beni <z004r0rk-a01@evosoft.com>
2023-08-25 09:50:33 +0200

    project is finished, time to style

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 const container = document.getElementById('container');
 
-const cols = 16;
-const height = 400/cols;
-const width = 400/cols;
+let cols = 5;
+let height = 400/cols;
+let width = 400/cols;
 
 container.style.cssText = `grid-template-columns: repeat(${cols}, ${width}px [col-start])`;
 
 for (let i = 0; i < cols*cols; i++) {
     const col = document.createElement('div');
     col.classList.add('col');
-    col.getAttribute('id', `col${i+1}`);
+    col.setAttribute('id', `col${i+1}`);
+    col.onmouseover = function() {
+        col.classList.add('colored');
+    };
     col.style.cssText = `width: ${width}px; min-height: ${height}px; clear:both`;
     container.appendChild(col);
+};
+
+document.getElementById('size').onclick = function() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    };
+    cols = prompt("Enter the number of blocks in a row!");
+    height = 400/cols;
+    width = 400/cols;
+    container.style.cssText = `grid-template-columns: repeat(${cols}, ${width}px [col-start])`;
+    for (let i = 0; i < cols*cols; i++) {
+        const col = document.createElement('div');
+        col.classList.add('col');
+        col.setAttribute('id', `col${i+1}`);
+        col.onmouseover = function() {
+            col.classList.add('colored');
+        };
+        col.style.cssText = `width: ${width}px; min-height: ${height}px; clear:both`;
+        container.appendChild(col);
+    };
 };
