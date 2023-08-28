c32d69d18e959f7ef44c61fbda28d3816d92b913
bainuo <97295550+fengbainuo@users.noreply.github.com>
2023-08-28 11:17:36 +0200

    testing

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const container = document.getElementById('container');
 
 let cols = 16;
-getSizes(cols);
 
 let color = "black";
 
-container.style.cssText = `grid-template-columns: repeat(${cols}, ${width}px [col-start])`;
+container.style.cssText = `grid-template-columns: repeat(${cols}, ${getSizes(cols)}px)`;
 
-generateGrid(cols, height, width);
+generateGrid(cols, getSizes(cols));
 
 document.getElementById('colorPicker').oninput = function(e) {
     color = e.target.value;
@@ -31,8 +30,8 @@ document.getElementById('size').onclick = function() {
     };
     cols = prompt("Enter the number of blocks in a row!");
     getSizes(cols);
-    container.style.cssText = `grid-template-columns: repeat(${cols}, ${width}px [col-start])`;
-    generateGrid(cols, height, width);
+    container.style.cssText = `grid-template-columns: repeat(${cols}, ${getSizes(cols)}px [col-start])`;
+    generateGrid(cols, getSizes(cols));
 };
 
 document.getElementById('colorChange').onclick = function() {
@@ -40,11 +39,10 @@ document.getElementById('colorChange').onclick = function() {
 };
 
 function getSizes(x) {
-    height = 400/x;
-    width = 400/x;
-};
+    return 400 / x
+}
 
-function generateGrid(cols, height, width) {
+function generateGrid(cols, size) {
     for (let i = 0; i < cols*cols; i++) {
         const col = document.createElement('div');
         col.classList.add('col');
@@ -52,9 +50,9 @@ function generateGrid(cols, height, width) {
         col.onmouseover = function(e) {
             if (e.buttons == 1 || e.buttons == 3) {
                 col.style.backgroundColor = color;
-            };
+            }
         };
-        col.style.cssText = `width: ${width}px; min-height: ${height}px; clear:both`;
+        col.style.cssText = `width: ${size}px; min-height: ${size}px; clear:both`;
         container.appendChild(col);
-    };
+    }
 }
